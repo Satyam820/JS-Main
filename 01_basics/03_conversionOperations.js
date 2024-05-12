9dfb094ca3581461efa09b671597c26b1a279568
Satyam820 <146512074+Satyam820@users.noreply.github.com>
2024-05-12 09:23:23 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+let score = null
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);    //NaN (not an number)
+
+
+//"33" => 33
+//"33abc" => Nan
+//for null => 0
+//for undefined => NaN
+// boolean true => 1
+// string => Nan
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+// 1 => True; 0 => False
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
