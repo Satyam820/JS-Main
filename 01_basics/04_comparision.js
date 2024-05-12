ea2293c88e60d6373de568e863d37ad4a85ad41d
Satyam820 <146512074+Satyam820@users.noreply.github.com>
2024-05-12 11:05:47 +0000

    Comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,20 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);       // number = number  or string = string (best conversion)
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+// // console.log(null > 0);      // Confusing
+// console.log(null == 0);   // in equality null is treat as undefined
+// console.log(null >= 0);   //True bcz comparision treat null as number, treating it as 0.
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+// ===
+
+console.log("2" === 2); // ==== check datatypes
